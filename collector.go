dcfc76bbe764262a63761a3b89ab07e63b688401
Ben Kochie <superq@gmail.com>
2018-11-04 17:05:59 +0100

    Use automatic exponential buckets
    Generate 20 exponential buckets from 50us.

--- collector.go
+++ collector.go
@@ -11,27 +11,6 @@ const (
 	namespace = "smokeping"
 )
 
-func secondsDurationBuckets() []float64 {
-	return []float64{
-		0.00005, /* 50us */
-		0.0001,  /* 100us */
-		0.00025, /* 250us */
-		0.0005,  /* 500us */
-		0.001,   /* 1ms */
-		0.0025,  /* 2.5ms */
-		0.005,   /* 5ms */
-		0.025,   /* 25ms */
-		0.1,     /* 100ms */
-		0.25,    /* 250ms */
-		0.5,     /* 500ms */
-		1.0,     /* 1s */
-		10.0,    /* 10s */
-		30.0,    /* 30s */
-		60.0,    /* 1m */
-		300.0,   /* 10m */
-	}
-}
-
 var (
 	labelNames = []string{"ip", "host"}
 
@@ -40,7 +19,7 @@ var (
 			Namespace: namespace,
 			Name:      "response_duration_seconds",
 			Help:      "A histogram of latencies for ping responses.",
-			Buckets:   secondsDurationBuckets(),
+			Buckets:   prometheus.ExponentialBuckets(0.00005, 2, 20),
 		},
 		labelNames,
 	)
